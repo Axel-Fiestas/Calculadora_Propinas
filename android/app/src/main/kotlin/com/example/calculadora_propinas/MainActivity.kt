1c29a70e6b5b65813d0742845082ddb4786d98d2
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2023-05-26 13:53:32 -0500

    FirstVersion

---
+++ android/app/src/main/kotlin/com/example/calculadora_propinas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calculadora_propinas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CalculadoraPropinasApp());
+}
+
+//Calcular automaticamente la propina segun un porcentaje seleccionado
+//Mostrar: Monto total a pagar (incluida la propina)
+//Debe permitir al usuario ajustar el porcentaje de propina y ver como cambia el monto total
+
+class CalculadoraPropinasApp extends StatelessWidget {
+  const CalculadoraPropinasApp ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Calculadora de Propinas Application",
+      theme: ThemeData(
+          primaryColor: Colors.orange,
+          primarySwatch: Colors.orange
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/':(context)=>const HomePage(),
+        '/calculator':(context)=>const CalculatorPage()
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Calculator your bribe!'),
+        ),
+        body: Stack(
+            fit: StackFit.expand,
+            children: <Widget> [
+              Image.network(
+                'http://m.gettywallpapers.com/wp-content/uploads/2022/06/Money-Wallpaper.jpg',
+                fit: BoxFit.cover,
+                color: Colors.black54,
+                colorBlendMode: BlendMode.darken,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(
+                    'https://cdn4.iconfinder.com/data/icons/stock-investment-filloutline/64/Insider-trading-stock-bribe-illegal-512.png',
+                    width: 300,
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+                      //Navigator.push(context, MaterialPageRoute(builder: (context)=>CalculatorPage())
+                      Navigator.pushNamed(context, '/calculator');
+                    },
+                    child: Text('Empezar!'))
+                ],
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+
+class CalculatorPage extends StatelessWidget {
+  const CalculatorPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.attach_money,
+                  color: Colors.orange,
+                  size: 50.0,
+                  semanticLabel: 'Money Icon for decorative objectives',
+                ),
+                SizedBox(height: 50,),
+                Text('Estamos en la pagina de Calculator'),
+                ElevatedButton(
+                    onPressed: (){
+                  Navigator.pop(context);
+                },child: Text('Go Back'))
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
